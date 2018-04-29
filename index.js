21ebaa1ff694db04f36fe6c92f647cba75ab83e4
Logan Ripplinger <logan.ripplinger@gmail.com>
2018-04-29 15:49:55 -0700

    Mostly working now. Choose random answer, plays most minigames too.

--- index.js
+++ index.js
@@ -6,8 +6,9 @@ const WebSocket = require('ws');
 var userId = '656fb518-d0ae-494f-bfd6-4e9a8fbde2fd'
 
 var user_name = 'ROBOT'
-var room = 'PYKN'
-var joinType = 'player' //'audience'
+var room = 'HTPB'
+var joinType = 'player' 
+// var joinType = 'audience' 
 
 jb.getWsUrl(userId, room).then(res => {
 
@@ -44,19 +45,20 @@ jb.getWsUrl(userId, room).then(res => {
 		    // done
 		  }
 		})
-	    console.log('Recieved: '+event.data);
 	    if (event.data.substring(0,1) === '2') {
-	    	console.log('pong!')
+	    	console.log('Ping? Pong!')
 	    	ws.send('2::')
+	    	return
 	    }
 
+	    console.log('\x1b[37m'+event.data+'\x1b[0m');
 	    if (event.data.substring(0,1) === '5') {
 			console.log('')	    	
 	    	// easiest method to check for trivia questions
 	    	try {
 
 		    	const json = JSON.parse(event.data.split(':::')[1])
-		    	console.log(json)
+		    	// console.log(json)
 		    	
 				if ('success' in json.args[0]) {
 		    		console.log('Recieved success: ' + json.args[0].success)
@@ -87,59 +89,119 @@ jb.getWsUrl(userId, room).then(res => {
 
 })
 
-function handlePlayer(ws, json) {
-	process.exit()
+function handlePlayer(ws, data) {
+	try {
+
+		//{"name":"msg","args":[{"type":"Event","event":"CustomerBlobChanged","roomId":"BWKS","blob":{"choices":[{"disabled":false,"text":"Coca-Cola"},{"disabled":false,"text":"Nestle"},{"disabled":false,"text":"Nabisco"},{"disabled":false,"text":"Heinz"}],"chosen":null,"dollInfo":{"controllerColors":{"dark":"#5f3c28","light":"#e1af78"},"name":"Orange"},"playerIndex":1,"playerName":"Im a robot","state":"MakeSingleChoice","text":"What food company actually owns the Weight Watchers weight loss clinics?"}}]}
+		json = data.blob
+
+		var choices = json.choices
+		// choices
+		// [ { disabled: false, text: 'Coca-Cola' },
+		//  { disabled: false, text: 'Nestle' },
+	  	//	{ disabled: false, text: 'Nabisco' },
+	  	//	{ disabled: false, text: 'Heinz' } ]
+
+	  	//TODO dont ignore disabled
+
+		var mode = json.state // MakeSingleChoice
+
+	  	if (mode === 'MakeSingleChoice') {
+	  		//push to a list
+	  		//pick a random one
+	  		var answer = Math.floor(Math.random() * choices.length)
+	  		//submit it
+	  		const message = '5:::{"name":"msg","args":[{"roomId":"' + room + '","userId":"' + userId + '","message":{"choice":' + answer + '},"type":"Action","appId":"87fd7112-e835-4794-88bc-dc6e3630d640","action":"SendMessageToRoomOwner"}]}'
+	  		console.log('Sending: ' + message)
+
+	  		var seconds = 3
+	  		var waitTill = new Date(new Date().getTime() + seconds * 1000);
+	  		while(waitTill > new Date()){}
+
+	  		ws.send(message)
+	  		return 
+	  	} else if (mode === 'MakeManyChoices')
+	  		const message = '5:::{"name":"msg","args":[{"roomId":"' + room + '","userId":"' + userId + '","message":{"choices":[' + answer + ']},"type":"Action","appId":"87fd7112-e835-4794-88bc-dc6e3630d640","action":"SendMessageToRoomOwner"}]}'
+
+	  	} else if (mode === 'Lobby') {
+	  		console.log('Lobby stuff. Ignoring.')
+			//"lobbyState":"CanStart",
+	  		//"lobbyState":"Countdown"
+	  		return
+
+	  	} else {
+	  		console.log('FOUND A DIFFERENT CHOICE: ' + mode)
+	  	}
+	} catch(e) {
+		console.log(e)
+	}
 }
+
+//cut finger off
+//5:::{"name":"msg","args":[{"type":"Event","event":"CustomerBlobChanged","roomId":"QNCT","blob":{"choices":[{"disabled":false,"text":"Cut finger 1"},{"disabled":false,"text":"Cut finger 2"},{"disabled":false,"text":"Cut finger 3"},{"disabled":false,"text":"Cut finger 4"}],"chosen":null,"dollInfo":{"controllerColors":{"dark":"#383838","light":"#afafaf"},"name":"Gray"},"playerIndex":1,"playerName":"Robot","state":"MakeSingleChoice","text":"Choose which finger to cut off"}}]}
+
 
 function handleAudience(ws, json) {
 
-		    	const trivia_data = json['blob']['audience']
+	if (!('blob' in json) || !('audience' in json['blob']) || !('choices' in json['blob']['audience'])) {
+		console.log('Does not contain a question/answer for audience. Ignoring.')
+		return
+	}
 
-		    	const question = trivia_data.text
-		    	const question_type = trivia_data.type
+	// console.log(!('blob' in json))
+	// console.log(!('audience' in json['blob']))
+	// console.log(!('choices' in json['blob']['audience']))	
 
-		    	const answer_list = trivia_data['choices']
+	const trivia_data = json.blob.audience
 
-		    	// for (i=1;i<=answer_list.length;i++) {
-		    	// 	console.log('real keys: ' + answer_list[i].key)
-		    	// }
+	const question = trivia_data.text
+	const question_type = trivia_data.type
 
-		    	let answer = []
-		    	for (let key of answer_list.keys()) {
-		    		console.log('answer key: ' + key)
-		    		console.log('answer text: ' + answer_list[key]['text'])
-		    		answer.push(answer_list[key]['text']) // can have <i> if title. TODO select key correctly
-		    	}
+	const answer_list = trivia_data.choices //array of objects
+	// console.log('answer_list: ' + answer_list[1])
 
-		    	console.log("Question: " + question)
-		    	console.log("Answers: " + answer)
+	let answer = []
+	for (i=0;i<answer_list.length;i++) {
+		if ('key' in answer_list[i]) {
+			answer.push(answer_list[i].key) //used when voting for users ids
+		} else {
+			answer.push(answer_list[i].text)
+		}
+	}
 
-		    	if (question_type === 'single') {
-			    	// send a random answer
-			    	var chosen_answer = answer[Math.floor(Math.random() * answer.length)];
-					const answer_message = '5:::{"name":"msg","args":[{"userId":"' + userId + '","roomId":"' + room + '","module":"vote","name":"Trivia Death Vote","message":{"type":"vote","vote":"' + chosen_answer + '"},"type":"Action","appId":"87fd7112-e835-4794-88bc-dc6e3630d640","action":"SendSessionMessage"}]}'
-					console.log('sending : ' + answer_message)
-					ws.send(answer_message)
-				} else if (question_type === 'multiple') {
-					// format, "vote":"true,true,false"
-					// bundle up those answers
-					var multi_answer = ''
-					for (i = 1; i<=answer.length; i++) {
-						var true_or_false = (Math.floor(Math.random() * 2) === 1)
-						if (i === 1) {
-							multi_answer = true_or_false
-						} else {
-							multi_answer = multi_answer + ',' + true_or_false
-						}
-					}
+	// for (let key of answer_list.keys()) {
+	// 	console.log('answer key: ' + key)
+	// 	console.log('answer text: ' + answer_list[key]['text'])
+	// 	answer.push(answer_list[key]['text']) // can have <i> if title. TODO select key correctly
+	// }
 
-					console.log(multi_answer)
+	console.log("Question: " + question)
+	console.log("Answers: " + answer)
 
-					// send those answers
-					const answer_message = '5:::{"name":"msg","args":[{"userId":"' + userId + '","roomId":"' + room + '","module":"vote","name":"Trivia Death Vote","message":{"type":"vote","vote":"' + multi_answer + '"},"type":"Action","appId":"87fd7112-e835-4794-88bc-dc6e3630d640","action":"SendSessionMessage"}]}'
-					console.log('sending : ' + answer_message)
-					ws.send(answer_message)
+	if (question_type === 'single') {
+    	// send a random answer
+    	var chosen_answer = answer[Math.floor(Math.random() * answer.length)];
+		const answer_message = '5:::{"name":"msg","args":[{"userId":"' + userId + '","roomId":"' + room + '","module":"vote","name":"Trivia Death Vote","message":{"type":"vote","vote":"' + chosen_answer + '"},"type":"Action","appId":"87fd7112-e835-4794-88bc-dc6e3630d640","action":"SendSessionMessage"}]}'
+		console.log('sending : ' + answer_message)
+		ws.send(answer_message)
+	} else if (question_type === 'multiple') {
+		// format, "vote":"true,true,false"
+		// bundle up those answers
+		var multi_answer = ''
+		for (i = 1; i<=answer.length; i++) {
+			var true_or_false = (Math.floor(Math.random() * 2) === 1)
+			if (i === 1) {
+				multi_answer = true_or_false
+			} else {
+				multi_answer = multi_answer + ',' + true_or_false
+			}
+		}
 
-				}
+		console.log(multi_answer)
 
+		// send those answers
+		const answer_message = '5:::{"name":"msg","args":[{"userId":"' + userId + '","roomId":"' + room + '","module":"vote","name":"Trivia Death Vote","message":{"type":"vote","vote":"' + multi_answer + '"},"type":"Action","appId":"87fd7112-e835-4794-88bc-dc6e3630d640","action":"SendSessionMessage"}]}'
+		console.log('sending : ' + answer_message)
+		ws.send(answer_message)
+	}
 }
