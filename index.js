03887e015b78501d8b1c53821c0f4136a3879025
Logan Ripplinger <logan.ripplinger@gmail.com>
2018-04-28 22:48:51 -0700

    Add parsing questions and answers. Exit process on game over.

--- index.js
+++ index.js
@@ -1,33 +1,81 @@
-// var jb = require("./jackbox_connection")
+var jb = require("./jackbox_connection")
 const WebSocket = require('ws');
 
 
-let ws_url = 'wss://i-0beef755e843a2ac4.jackboxgames.com:38203/socket.io/1/websocket/QorxKbnlfEOtULnTYcEC' // program parameter
-let host_url = 'i-0beef755e843a2ac4' + ":38203"
 
+var userId = '656fb518-d0ae-494f-bfd6-4e9a8fbde2fd'
+var room = 'IGHB'
 
-const ws = new WebSocket(ws_url, {
-	origin: "https://jackbox.tv",
-	host: host_url,
-	perMessageDeflate: false
-});
+jb.getWsUrl(userId, room).then(res => {
 
+	console.log(res)
+	if (res['success'] === 'fail') { 
+		console.log('Encountered a failure when attempting to join the room ' + room + '. Exiting.')
+		process.exit() 
+	}
+	
+	let ws_url = res['ws_url'] // program parameter
+	let host_url = res['serverid'] + ":38203"
 
-var openmessage = '5:::{"name":"msg","args":[{"roomId":"PNDO","name":"LOL","appId":"87fd7112-e835-4794-88bc-dc6e3630d640","joinType":"audience","options":{"roomcode":"PNDO","name":"LOL","email":"","phone":""},"type":"Action","userId":"656fb518-d0ae-494f-bfd6-4e9a8fbde2fd","action":"JoinRoom"}]}'
+	console.log(host_url)
 
-ws.onopen = function (event) {
-    console.log('Connection Open');
-	// ws.send('1::');
-	ws.send(openmessage)
-};
-ws.onmessage = function (event) {
-    console.log('Recieved: '+event.data);
-    if (event.data.substring(0,1) === '2') {
-    	console.log('pong!')
-    	ws.send('2::')
-    }
-};
+	const ws = new WebSocket(ws_url, {
+		origin: "https://jackbox.tv",
+		host: host_url,
+		perMessageDeflate: false
+	});
 
+	var openmessage = '5:::{"name":"msg","args":[{"roomId":"' + room + '","name":"LOL","appId":"87fd7112-e835-4794-88bc-dc6e3630d640","joinType":"audience","options":{"roomcode":"' + room + '","name":"LOL","email":"","phone":""},"type":"Action","userId":"656fb518-d0ae-494f-bfd6-4e9a8fbde2fd","action":"JoinRoom"}]}'
+
+	ws.onopen = function (event) {
+	    console.log('Connection Open');
+		// ws.send('1::');
+		ws.send(openmessage)
+		ws.send('5:::{"name":"msg","args":[{"roomId":"ARMA","name":"LOL","appId":"87fd7112-e835-4794-88bc-dc6e3630d640","joinType":"audience","options":{"roomcode":"ARMA","name":"LOL","email":"","phone":""},"type":"Action","userId":"656fb518-d0ae-494f-bfd6-4e9a8fbde2fd","action":"JoinRoom"}]}')
+	};
+
+	ws.onmessage = function (event) {
+	    console.log('Recieved: '+event.data);
+	    if (event.data.substring(0,1) === '2') {
+	    	console.log('pong!')
+	    	ws.send('2::')
+	    }
+
+	    if (event.data.substring(0,1) === '5') {
+			console.log('')	    	
+
+	    	// easiest method to check for trivia questions
+	    	try {
+		    	// TRIVIA QUESTIONS
+		    	const json = JSON.parse(event.data.split(':::')[1])
+		    	console.log(json)
+
+		    	// check if game over
+		    	if (json.args[0]['blob'].gameResults !== null) {
+		    		console.log('Game over, exiting')
+		    		process.exit()
+		    	}
+
+		    	const trivia_data = json.args[0]['blob']['audience']
+
+		    	const question = trivia_data.text
+
+		    	const answer_list = trivia_data['choices']
+		    	let answer = []
+		    	for (let key of answer_list.keys()) {
+		    		answer.push(answer_list[key]['text']) // REMOVE HTML
+		    	}
+
+		    	console.log("Question: " + question)
+		    	console.log("Answers: " + answer)
+		    } catch(e) {
+		    	console.log('error on trying to extract trivia_data')
+		    	console.log(e)
+		    }
+	    }
+	};
+
+})
 
  
 // ws.on('message', function incoming(data) {
--- jackbox_connection.js
+++ jackbox_connection.js
@@ -1,44 +1,7 @@
 const axios = require('axios');
 
-// const example = require('example-library');
-
-// const runDemo = async () => {
-//   try {
-//     const fistResponse = await example.firstAsyncRequest();
-//     const secondResponse = await example.secondAsyncRequest(fistResponse);
-//     const thirdAsyncRequest = await example.thirdAsyncRequest(secondResponse);
-//   }
-//   catch (error) {
-//     // Handle error
-//   }
-// };
-
-// runDemo();
-
-async function go() {
-  try {
-    // but first, coffee
-    const coffee = await getCoffee();
-    console.log(coffee); // ☕
-    // then we grab some data over an Ajax request
-    const wes = await axios('https://api.github.com/users/wesbos');
-    console.log(wes.data); // mediocre code
-    // many requests should be concurrent - don't slow things down!
-    // fire off three requests and save their promises
-    const wordPromise = axios('http://www.setgetgo.com/randomword/get.php');
-    const userPromise = axios('https://randomuser.me/api/');
-    const namePromise = axios('https://uinames.com/api/');
-    // await all three promises to come back and destructure the result into their own variables
-    const [word, user, name] = await Promise.all([wordPromise, userPromise, namePromise]);
-    console.log(word.data, user.data, name.data); // cool, {...}, {....}
-  } catch (e) {
-    console.error(e); // 💩
-  }
-}
-
-async function firstConnection(room) {
+async function firstConnection(userId, room) {
 	try {
-		var userId = '656fb518-d0ae-494f-bfd6-4e9a8fbde2fd'
 		var url = 'https://blobcast.jackboxgames.com/room/' + room + '?userId=' + userId
 
 		const res = await axios.get(url);
@@ -52,14 +15,10 @@ async function firstConnection(room) {
 
 		var serverid = res.data.server
 	    
-	    switch(res.data.joinAs){
-	    	case "full":
-	    		return ["full", ""]
-	    	case "audience":
-	    		return ["audience", serverid]
-	    	case "player":
-	    		return ["player", serverid]
-	    }
+	    return {
+	    	'mode': res.data.joinAs,
+	    	'serverid': res.data.server
+		}
 	} catch (e) {
 		console.log(e)
 	}
@@ -82,23 +41,30 @@ async function secondConnection(serverid) {
 }
 
 
-async function getWsUrl(room) {
+async function getWsUrl(userId, room) {
 	try {
-		const serverid = await firstConnection(room);
-		if (serverid[0] === "full") {
-			return ['fail', '']
+		
+		const data = await firstConnection(userId, room);
+
+		//fail if we are joining as a player or the audience is disabled
+		if (data['mode'] === "full" || data['mode'] === "player") { 
+			return {'success': 'fail','mode': data['mode'], 'ws_url': '','serverid': ''}
 	  	}
 
-		console.log('got first wait, serverid is :' + serverid[1])
-		const ws_url = await secondConnection(serverid[1]);
+		console.log('got first wait, serverid is :' + data['serverid'])
+		const ws_url = await secondConnection(data['serverid']);
 
-		return ['success', ws_url, serverid[0]]
+		return {
+			'success': 'success',
+			'mode': data['mode'],
+			'ws_url': ws_url,
+			'serverid': data['serverid']
+		}
 	} catch(e) {
-		return ['fail', '']
+		//todo if 404 return room not found
+		console.log(e)
+		return {'success': 'fail','mode':'','ws_url': '','serverid': ''}
 	}
 }
 
-// module.exports.getWsUrl = getWsUrl;
-getWsUrl('PNDO').then(v => {
-	console.log(v)
-})
+module.exports.getWsUrl = getWsUrl;
