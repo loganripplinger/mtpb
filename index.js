c8303e8ce5ed4059e84b27536962f0318a7a00a8
Logan Ripplinger <logan.ripplinger@gmail.com>
2018-05-01 12:41:09 -0700

    ...

--- index.js
+++ index.js
@@ -1,28 +1,43 @@
-const { spawn } = require('child_process');
+var express = require('express');
+var app = express();
 
-function randRoom() {
-  var text = "";
-  var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+app.get('/', function(req, res){
+   res.send("Hello world!");
+});
 
-  for (var i = 0; i < 5; i++)
-    text += possible.charAt(Math.floor(Math.random() * possible.length));
 
-  return text;
-}
 
-for (i=1;i<=5;i++) {
-	const child = spawn('node', ['bot_join.js', randRoom()]);
 
-	child.stdout.on('data', (data) => {
-	  console.log(`child stdout:\n${data}`);
-	});
 
-	child.stderr.on('data', (data) => {
-	  console.error(`child stderr:\n${data}`);
-	});
+// const { spawn } = require('child_process');
 
-	child.on('exit', function (code, signal) {
-	  console.log('child process exited with ' +
-	              `code ${code} and signal ${signal}`);
-	});	
-}
+// function randRoom() {
+//   var text = "";
+//   var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+//   for (var i = 0; i < 5; i++)
+//     text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+//   return text;
+// }
+
+// for (i=1;i<=5;i++) {
+// 	const child = spawn('node', ['bot_join.js', randRoom()]);
+
+// 	child.stdout.on('data', (data) => {
+// 	  console.log(`child stdout:\n${data}`);
+// 	});
+
+// 	child.stderr.on('data', (data) => {
+// 	  console.error(`child stderr:\n${data}`);
+// 	});
+
+// 	child.on('exit', function (code, signal) {
+// 	  console.log('child process exited with ' +
+// 	              `code ${code} and signal ${signal}`);
+// 	});	
+// }
+
+
+
+app.listen(3000);
